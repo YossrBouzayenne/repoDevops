9c2649d685d3557096205c22fed4fee9ecb32349
YossrBouzayenne <47390569+YossrBouzayenne@users.noreply.github.com>
2022-03-01 16:33:04 +0100

    fix commit from uml

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -19,6 +19,7 @@ import javax.persistence.TemporalType;
 public class User implements Serializable {
 	//commentaire test git
 	//commentaire test 2
+	//commentaire push from eclipse 
 	
 	private static final long serialVersionUID = 1L;
 
