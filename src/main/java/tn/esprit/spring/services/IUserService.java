82482c7122a60ff06053339af7b3825b87087a79
YossrBouzayenne <47390569+YossrBouzayenne@users.noreply.github.com>
2022-06-03 23:09:39 +0200

    Add USer

--- src/main/java/tn/esprit/spring/services/IUserService.java
+++ src/main/java/tn/esprit/spring/services/IUserService.java
@@ -7,7 +7,7 @@ import tn.esprit.spring.entities.User;
 public interface IUserService { 
 	 
 	List<User> retrieveAllUsers(); 
-	//User addUser(User u);
+	User addUser(User u);
 	void deleteUser(String id);
 	User updateUser(User u);
 	User retrieveUser(String id);
